39daed282b5944fc09e1dcb10471307e63aa52d1
SweathaBharathi <sweatha.bharathi@syncfusion.com>
2025-06-24 13:55:02 +0530

    To change Gridline color in a WPF SfTreeGrid

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# How-to-change-gridline-color-in-a-WPF-TreeGrid
-This demo shows how to change gridline color in a WPF TreeGrid
+# How to change gridline color in a WPF TreeGrid
+
+In [WPF SfTreeGrid](https://www.syncfusion.com/wpf-controls/treegrid), the grid line color can be customized by setting the BorderBrush property on the [TreeGridCell](https://help.syncfusion.com/cr/wpf/Syncfusion.UI.Xaml.TreeGrid.TreeGridCell.html), [TreeGridHeaderCell](https://help.syncfusion.com/cr/wpf/Syncfusion.UI.Xaml.TreeGrid.TreeGridHeaderCell.html), [TreeGridExpanderCell](https://help.syncfusion.com/cr/wpf/Syncfusion.UI.Xaml.TreeGrid.TreeGridExpanderCell.html), and the SfTreeGrid.
+
+ ```xml
+<Window.Resources>
+    <!--To change the grid line color for the TreeGridCell-->
+    <Style TargetType="syncfusion:TreeGridCell">
+        <Setter Property="BorderBrush" Value="Green" />
+    </Style>
+    <!--To change the grid line color for the TreeGridHeaderCell-->
+    <Style TargetType="syncfusion:TreeGridHeaderCell">
+        <Setter Property="BorderBrush" Value="Green" />
+    </Style>
+    <!--To Change the grid line color for the TreeGridExpanderCell-->
+    <Style TargetType="syncfusion:TreeGridExpanderCell">
+        <Setter Property="BorderBrush" Value="Green" />
+    </Style>
+</Window.Resources>
+
+<Grid>
+    <syncfusion:SfTreeGrid Name="treeGrid"    
+                           BorderBrush="Green"
+                           ChildPropertyName="ReportsTo"
+                           ItemsSource="{Binding Employees}"
+                           ParentPropertyName="ID"
+                           AllowEditing="True"
+                           SelfRelationRootValue="-1">
+    </syncfusion:SfTreeGrid>
+</Grid> 
+ ```
+ ![ChangingGridLineColor.png](ChangingGridLineColor.png)
+
+Take a moment to peruse the [WPF SfTreeGrid - GridLines](https://help.syncfusion.com/wpf/treegrid/gridlines) documentation, to learn more about SfTreeGrid GridLines customization with examples.
---
+++ SfTreeGridDemo/Model/Model.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel;
+
+namespace SfTreeGridDemo
+{
+    public class EmployeeInfo : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private string _title;
+        private double? _salary;
+        private int _reportsTo;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public int ID
+        {
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(ID));
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    OnPropertyChanged(nameof(FirstName));
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    OnPropertyChanged(nameof(LastName));
+                }
+            }
+        }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    OnPropertyChanged(nameof(Title));
+                }
+            }
+        }
+
+        public double? Salary
+        {
+            get => _salary;
+            set
+            {
+                if (_salary != value)
+                {
+                    _salary = value;
+                    OnPropertyChanged(nameof(Salary));
+                }
+            }
+        }
+
+        public int ReportsTo
+        {
+            get => _reportsTo;
+            set
+            {
+                if (_reportsTo != value)
+                {
+                    _reportsTo = value;
+                    OnPropertyChanged(nameof(ReportsTo));
+                }
+            }
+        }
+    }
+}
+    
+
+
